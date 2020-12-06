502f0054574e95a300257466992696aa9dff6450
Pedro Oliveira <pedrooliveira2007@hotmail.com>
2020-12-06 17:52:08 +0000

    planet and stars modified

--- Planet.cs
+++ Planet.cs
@@ -1,38 +1,98 @@
+using System;
+
 namespace LP2_Exoplanets_2020
 {
     public class Planet
     {
-        
+
         //star name
         public string Pl_name { get; set; }
-        //star effective temperature (kelvins)
-        public string HostName {get;set;}
-        public string Discoverymethod  { get; set; }
+        public string Discoverymethod { get; set; }
         //star radius (sun relation)
-        public float? Disc_year  { get; set; }
+        public float Disc_year { get; set; }
         // Star mass (sun relation)
-        public float? Pl_orbper { get; set; }
+        public float Pl_orbper { get; set; }
         //star age (Giga-years)
-        public float? Pl_rade  { get; set; }
+        public float Pl_rade { get; set; }
         //star rotational velocity (km/s)
-        public float? Pl_masse  { get; set; }
+        public float Pl_masse { get; set; }
         //star rotational period (days)
-        public float? Pl_eqt  { get; set; }
+        public float Pl_eqt { get; set; }
 
-        public Planet(
-             string pl_name, string hostName, string discoveryMethod = default, 
-            float disc_year = default, float pl_orbper = default,
-            float pl_rade = default, float pl_masse = default, 
-            float pl_eqt = default)
+        public Star HostStar { get; set; }
+
+        public Planet(string[] fields, string[] fieldsOrder)
         {
-            Pl_name = pl_name;
-            HostName = hostName;
-            Discoverymethod = discoveryMethod;
-            Disc_year = disc_year;
-            Pl_orbper = pl_orbper;
-            Pl_rade = pl_rade;
-            Pl_masse = pl_masse;
-            Pl_eqt = pl_eqt;
+            for (int i = 0; i < fieldsOrder.Length; i++)
+            {
+
+                switch (fieldsOrder[i])
+                {
+
+                    case "pl_name":
+                        Pl_name = fields[i];
+                        break;
+
+                    case "hostname":
+                        HostStar.StarName = fields[i];
+                        break;
+
+                    case "discoverymethod":
+                        Discoverymethod = fields[i];
+                        break;
+
+                    case "pl_orbper":
+                        Pl_orbper = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "pl_rade":
+                        Pl_rade = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "pl_masse":
+                        Pl_masse = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "pl_eqt":
+                        Pl_eqt = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "st_teff":
+                        HostStar.St_teff = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "st_rad":
+                        HostStar.St_rad = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "st_mass":
+                        HostStar.St_mass = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "st_age":
+                        HostStar.St_age = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "st_vsin":
+                        HostStar.St_vsin = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "st_rotp":
+                        HostStar.St_rotp = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+                    case "sy_dist":
+                        HostStar.Sy_dist = fields[i] != null ? (float)Convert.ToDouble(fields[i]) : 0f;
+                        break;
+
+
+                }
+
+
+            }
+
+        Console.WriteLine("planet {0} generated.",Pl_name);
+
         }
 
 
--- Star.cs
+++ Star.cs
@@ -3,38 +3,21 @@ namespace LP2_Exoplanets_2020
     public class Star
     {
         //star name
-        public string StarName { get; set; }
+        public string StarName { get; internal set; }
         //star effective temperature (kelvins)
-        public float? St_teff { get; set; }
+        public float St_teff { get;internal set; }
         //star radius (sun relation)
-        public float? St_rad { get; set; }
+        public float St_rad { get;internal set; }
         // Star mass (sun relation)
-        public float? St_mass { get; set; }
+        public float St_mass { get; internal set; }
         //star age (Giga-years)
-        public float? St_age { get; set; }
+        public float St_age { get; internal set; }
         //star rotational velocity (km/s)
-        public float? St_vsin { get; set; }
+        public float St_vsin { get;internal set; }
         //star rotational period (days)
-        public float? St_rotp { get; set; }
+        public float St_rotp { get;internal set; }
         //star distance from Sun
-        public float? Sy_dist { get; set; }
-
-        public Star(
-            string hostName, float st_teff = default, 
-            float st_rad = default, float st_mass = default,
-            float st_age = default, float st_vsin = default, 
-            float st_rotp = default, float sy_dist = default)
-        {
-            StarName = hostName;
-            St_teff = st_teff;
-            St_rad = st_rad;
-            St_mass = st_mass;
-            St_age = st_age;
-            St_vsin = st_vsin;
-            St_rotp = st_rotp;
-            Sy_dist = sy_dist;
-        }
-
+        public float Sy_dist { get;internal set; }
 
     }
 }
